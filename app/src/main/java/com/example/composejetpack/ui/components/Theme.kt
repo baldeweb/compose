37a4318e85fab7077924b1ca7fde92426fdd7d3b
wallace <wallacecorin@gmaik,cin>
2021-05-16 23:37:19 -0300

    creating the login screen of BTG+ application

--- app/src/main/java/com/example/composejetpack/ui/components/Theme.kt
+++ app/src/main/java/com/example/composejetpack/ui/components/Theme.kt
@@ -1,45 +1,44 @@
-package com.example.composejetpack
+package com.example.composejetpack.ui.components
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
+    primaryVariant = ice,
+    secondary = teal200,
+    background = softBlack,
 )
 
 private val LightColorPalette = lightColors(
     primary = purple500,
-    primaryVariant = purple700,
+    primaryVariant = ice,
     secondary = teal200,
     background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
+    surface = Color.Black,
+    onPrimary = Color.Black,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
 )
-
-@Composable
-fun JetpackComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
+//
+//@Composable
+//fun JetpackComposeTheme(
+//    darkTheme: Boolean,
+//    content: @Composable () -> Unit
+//) {
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+//
+//    MaterialTheme(
+//
+//        colors = colors,
+//        typography = typography,
+//        shapes = shapes,
+//        content = content
+//    )
+//}
