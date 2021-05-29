75ed4942ac89f87ac7f3baf058933fec697095a9
wallace <wallacecorin@gmaik,cin>
2021-05-29 18:48:19 -0300

    including AppToolBar

--- app/src/main/java/com/example/composejetpack/ui/MainActivity.kt
+++ app/src/main/java/com/example/composejetpack/ui/MainActivity.kt
@@ -7,14 +7,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,29 +45,38 @@ fun GreetingView() {
 
 @Composable
 fun Greeting(viewModel: SWViewModel?) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            modifier = Modifier.clip(shape = RoundedCornerShape(8.dp)),
-            painter = painterResource(id = R.drawable.androidicon),
-            contentDescription = ""
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                }
+            )
+        }) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.clip(shape = RoundedCornerShape(8.dp)),
+                painter = painterResource(id = R.drawable.androidicon),
+                contentDescription = ""
+            )
 
-        Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
 
-        viewModel?.people?.observeAsState()?.value?.name?.let {
-            Text(text = "Name:", style = MaterialTheme.typography.h6)
-            Text(text = it, style = MaterialTheme.typography.body1)
+            viewModel?.people?.observeAsState()?.value?.name?.let {
+                Text(text = "Name:", style = MaterialTheme.typography.h6)
+                Text(text = it, style = MaterialTheme.typography.body1)
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            ListMovies(viewModel)
         }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        ListMovies(viewModel)
     }
 }
 
